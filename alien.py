b377fb2d305e371c0d6a5c16e55ee2fba6959367
dhruv saxena <dhruv12v34@gmail.com>
2025-08-21 22:43:49 +0530

    added to github

---
+++ alien.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Load the alien image and set its rect.
+        # Instead of loading an image, make a green rectangle for testing
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
+
